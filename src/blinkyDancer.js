46c2786958e2efd300079b727fe8af3f6d3bbb78
Hack Reactor Student <student@hackreactor.com>
2017-10-09 19:47:28 -0500

    passing two tests in SpecRunner, but red dots are not blinking in Object Dance Party

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,5 +1,37 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-  var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
+  makeDancer.call(this, top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  var oldStep = this.step;
+
+  this.step = function() {
+    // call the old version of step at the beginning of any call to this new version of step
+    oldStep.call(makeDancer);
+    console.log('12:', this.$node)
+    var x = this;
+    var tog = function() {
+      console.log('15:', x.$node)
+      x.$node.toggle();
+      setTimeout(function() { tog(); }, timeBetweenSteps);
+    };
+    tog();
+    // toggle() is a jQuery method to show/hide the <span> tag.
+    // See http://api.jquery.com/category/effects/ for this and
+    // other effects you can use on a jQuery-wrapped html tag.
+    
+  };
+
+  // return blinkyDancer;
+};
+
+makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
+/*
+var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  //makeDancer.call(this);
+  var blinkyDancer = makeDancer.call(this, top, left, timeBetweenSteps);
+
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
@@ -15,5 +47,7 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
     blinkyDancer.$node.toggle();
   };
 
-  return blinkyDancer;
 };
+*/
+
+makeBlinkyDancer.constructor = makeBlinkyDancer;
--- src/dancer.js
+++ src/dancer.js
@@ -1,19 +1,48 @@
-// Creates and returns a new dancer object that can step
+// Creates and returns a new dancer object that can step 
 var makeDancer = function(top, left, timeBetweenSteps) {
 
-  var dancer = {};
+  // var dancer = {};
 
   // use jQuery to create an HTML <span> tag
-  dancer.$node = $('<span class="dancer"></span>');
+  this.$node = $('<span class="dancer"></span>');
 
-  dancer.step = function() {
+  this.step = function() {
     // the basic dancer doesn't do anything interesting at all on each step,
     // it just schedules the next step
-    setTimeout(dancer.step, timeBetweenSteps);
+    setTimeout(function() { this.step; }, timeBetweenSteps);
   };
-  dancer.step();
 
-  dancer.setPosition = function(top, left) {
+  this.step();
+
+  this.setPosition = function(top, left) {
+  // Use css top and left properties to position our <span> tag
+  // where it belongs on the page. See http://api.jquery.com/css/
+    //
+    var styleSettings = {
+      top: top,
+      left: left
+    };
+    this.$node.css(styleSettings);
+  };
+
+  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
+  // this one sets the position to some random default point within the body
+  this.setPosition(top, left);
+
+  // return dancer;
+
+};
+
+
+// Creates and returns a new dancer object that can step
+/*
+var makeDancer = function(top, left, timeBetweenSteps) {
+
+  // use jQuery to create an HTML <span> tag
+  this.$node = $('<span class="dancer"></span>');
+  this.step();
+
+  this.setPosition = function(top, left) {
     // Use css top and left properties to position our <span> tag
     // where it belongs on the page. See http://api.jquery.com/css/
     //
@@ -21,12 +50,19 @@ var makeDancer = function(top, left, timeBetweenSteps) {
       top: top,
       left: left
     };
-    dancer.$node.css(styleSettings);
+    this.$node.css(styleSettings);
   };
 
-  // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
+  // now that we have defined the this object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
-  dancer.setPosition(top, left);
-
-  return dancer;
+  this.setPosition(top, left);
 };
+
+*/
+makeDancer.prototype.step = function() {
+  setTimeout(dancer.step, timeBetweenSteps);
+};
+
+
+
+
